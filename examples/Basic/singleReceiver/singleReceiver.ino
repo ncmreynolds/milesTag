e0485a40f66bea7acec2203928c93ae197f551b6
Nick Reynolds <ncmreynolds@googlemail.com>
2024-10-14 18:30:23 +0100

    Spruce up examples a bit

--- examples/Basic/singleReceiver/singleReceiver.ino
+++ examples/Basic/singleReceiver/singleReceiver.ino
@@ -6,7 +6,7 @@
 
 void setup() {
   Serial.begin(115200);                   //Set up Serial for debug output
-  milesTag.debug(Serial);                 //Send milesTag debug output to Serial (optional)
+  //milesTag.debug(Serial);                 //Send milesTag debug output to Serial (optional)
   milesTag.begin(milesTag.receiver);      //Simple single receiver requires basic initialisation
   milesTag.setReceivePin(34);             //Set the receive pin, which is mandatory
 }
@@ -14,7 +14,19 @@ void setup() {
 void loop() {
   if(milesTag.dataReceived())             //There is something in the packet buffer of the first 'busy' receiver. Multiple receivers can be busy and are handled individually.
   {
-    Serial.println(F("Data"));
+    Serial.print(F("Received "));
+    if(milesTag.receivedDamage())
+    {
+      Serial.print(milesTag.receivedDamage());
+      Serial.print(F(" damage from player ID:"));
+      Serial.print(milesTag.receivedPlayerId());
+      Serial.print(F(" team ID:"));
+      Serial.println(milesTag.receivedTeamId());
+    }
+    else
+    {
+      Serial.println(F("message"));
+    }
     milesTag.resumeReception();           //Clear the first 'busy' receiver buffer and prepare it for another packet. It is not automatically cleared
   }
 }
--- examples/Basic/singleTransmitter/singleTransmitter.ino
+++ examples/Basic/singleTransmitter/singleTransmitter.ino
@@ -6,7 +6,7 @@
 
 void setup() {
   Serial.begin(115200);                   //Set up Serial for debug output
-  milesTag.debug(Serial);                 //Send milesTag debug output to Serial (optional)
+  //milesTag.debug(Serial);                 //Send milesTag debug output to Serial (optional)
   milesTag.begin();                       //Simple single transmitter requires no other initialisation
   milesTag.setTransmitPin(12);            //Set the transmit pin, which is mandatory
   milesTag.setPlayerId(random(0,128));    //Set random player ID 0-127
@@ -14,6 +14,7 @@ void setup() {
 }
 
 void loop() {
+  Serial.println(F("Firing random damage"));
   milesTag.transmitDamage(random(1,101)); //Transmit 1-100 damage from the first transmitter. Note milesTag only has 16 damage steps (1,2,4,5,7,10,15,17,20,25,30,35,40,50,75,100) so this will often be rounded down before sending
   delay(10e3);
 }
